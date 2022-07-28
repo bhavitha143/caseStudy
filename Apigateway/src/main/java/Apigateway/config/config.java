4f4692c342acd6595fc33349c72d4b90837b9ffb
bhavitha143 <95899163+bhavitha143@users.noreply.github.com>
2022-07-28 15:49:10 +0530

    Add files via upload

---
+++ Apigateway/src/main/java/Apigateway/config/config.java
@@ -0,0 +1,38 @@
+package Apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class config {
+	@Bean
+public RouteLocator allroutes(RouteLocatorBuilder route)
+	{
+	return route.routes()
+			.route(customer->customer
+		    .path("/user/*")
+		    .uri("http://localhost:8084"))
+			
+			.route(Admin->Admin
+			.path("/admin/*")
+			.uri("http://localhost:8080"))
+			
+			.route(washer->washer 
+			.path("/wash/*")
+			.uri("http://localhost:8085"))
+			
+			.route(order->order 
+			.path("/order/*")
+			.uri("http://localhost:8081"))
+
+			
+			.route(pay->pay
+			.path("/gateway/*")
+			.uri("http://localhost:1234"))
+		    .build();
+	
+}
+
+}
